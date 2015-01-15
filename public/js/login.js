cb35e678bde97d81abb7be9b5a5b9bd7ec329d91
Jingwei (Jason) Tu <jingweitu@berkeley.edu>
2015-01-15 17:58:29 -0500

    login

---
+++ public/js/login.js
@@ -0,0 +1,95 @@
+/**
+ * Created by Amanpreet on 1/2/15.
+ */
+
+var loginButton; 
+var signupButton; 
+var submitButton;
+var nameDiv;
+
+//0 for login, 1 for signup
+var curState;
+
+ $( document ).ready(function() {
+    
+  loginButton = $(".loginButton");
+  signupButton = $(".signupButton");
+  submitButton = $(".submitButton");
+  nameDiv = $(".nameDiv");
+  curState = 0;
+
+  loginButton.click(function(){
+        nameDiv.hide();
+        curState = 0;
+    });
+
+  signupButton.click(function(){
+        nameDiv.show();
+        curState = 1;
+    });
+
+  submitButton.click(function(){
+        if (curState == 0){
+          login();
+        }
+        else{
+          signup();
+        }
+    });  
+
+});
+
+//request for logging in
+
+function login(){
+
+  var email = $(".emailBox").val();
+  var pass = $(".passwordBox").val();
+
+  $.ajax({
+    type: "POST",
+    contentType: "application/json",
+    dataType: "json",
+    url: "/users/login/",
+    data: JSON.stringify({email : email, password : pass})
+}).done(function( msg ) {
+    debug = msg;
+    console.log(msg)
+    if (msg['status'] == 0){
+      alert(msg['message']);
+    }
+    else{
+      console.log('success');
+      //add a redirect here to profile page
+    }
+});
+}
+
+
+//request for signing up
+
+function signup(){
+
+  var name = $(".nameBox").val();
+  var email = $(".emailBox").val();
+  var pass = $(".passwordBox").val();
+
+  $.ajax({
+     type: "POST",
+     contentType: "application/json",
+     dataType: "json",
+     url: "/users/signup/",
+     data: JSON.stringify({name : 'myname', email : 'myemail', password : 'pass'})
+   }).done(function( msg ) {
+         debug = msg;
+         console.log(msg)
+         if (msg['status'] == 0){
+          alert(msg['message']);
+         }
+         else{
+          console.log('success');
+          //add a redirect here to profile page
+         }
+       });
+}
+
