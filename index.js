4f9ad1247138385d88f01f087d9e529ce4f30aeb
akandola <amanpreet123@gmail.com>
2015-01-15 17:11:50 -0500

    added login functions kinda

--- index.js
+++ index.js
@@ -57,3 +57,35 @@ app.get('/login.html', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+//Authorization functions
+function checkAuth(req, res, next) {
+  if (!req.session.user_id) {
+    res.send('You are not authorized to view this page');
+  } else {
+  	res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+  }
+}
+
+//login route
+app.post('/login', function (req, res) {
+  var post = req.body;
+  if (post.user === 'john' && post.password === 'johnspassword') {
+    req.session.user_id = johns_user_id_here;
+    res.redirect('/my_secret_page');
+  } else {
+    res.send('Bad user/pass');
+  }
+});
+
+//logout route
+app.get('/logout', function (req, res) {
+  delete req.session.user_id;
+  res.redirect('/login');
+});  
+
+//example
+app.get('/my_secret_page', checkAuth, function (req, res) {
+  res.send('if you are viewing this page it means you are logged in');
+});
