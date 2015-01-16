99addf4197e624b97700577b410a22598e8ba1f5
Jingwei (Jason) Tu <jingweitu@berkeley.edu>
2015-01-16 17:39:21 -0500

    edits

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(session({
   secret: 'snapGur',
   resave: false,
   saveUninitialized: true
-}))
+}));
+app.use(expressSession({secret: 'snapGur'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
