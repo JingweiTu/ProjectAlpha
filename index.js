d2636eb2551eade3a8282ee97edec730fe51f2a4
akandola <amanpreet123@gmail.com>
2015-01-15 20:54:25 -0500

    login

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(app.router);
 
 app.get('/', function(request, response) {
 	//console.log("request");
@@ -98,32 +97,32 @@ function checkAuth(req, res, next) {
 }
 
 
-router.get('/', function(req, res) {
+ app.get('/', function(req, res) {
     	// Display the Login page with any flash message, if any
 		res.render('index', { message: req.flash('message') });
 	});
 
 	/* Handle Login POST */
-	router.post('/login', passport.authenticate('login', {
+	 app.post('/login', passport.authenticate('login', {
 		successRedirect: '/',
 		failureRedirect: '/login.html',
 		failureFlash : true  
 	}));
 
 	/* GET Registration Page */
-	router.get('/signup', function(req, res){
+	 app.get('/signup', function(req, res){
 		res.render('register',{message: req.flash('message')});
 	});
 
 	/* Handle Registration POST */
-	router.post('/signup', passport.authenticate('signup', {
+	 app.post('/signup', passport.authenticate('signup', {
 		successRedirect: '/home',
 		failureRedirect: '/signup',
 		failureFlash : true  
 	}));
 
 	/* Handle Logout */
-	router.get('/signout', function(req, res) {
+	 app.get('/signout', function(req, res) {
 		req.logout();
 		res.redirect('/');
 	});
