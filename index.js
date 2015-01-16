7dc1576cfef60c8566d417ae720b2285e556f70d
akandola <amanpreet123@gmail.com>
2015-01-15 21:00:26 -0500

    login

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
 app.get('/', function(request, response) {
 	//console.log("request");
 	/*
@@ -96,6 +100,15 @@ function checkAuth(req, res, next) {
   }
 }
 
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
 
  app.get('/', function(req, res) {
     	// Display the Login page with any flash message, if any
