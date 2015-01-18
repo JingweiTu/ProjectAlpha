35597a95f67b5b4bc9881158eab9a779465fadff
Jingwei (Jason) Tu <jingweitu@berkeley.edu>
2015-01-18 02:06:52 -0500

    changed frontpage

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/frontpage.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuring Passport
 var passport = require('passport');
